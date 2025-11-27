817599481ea6a2cad9dcc3db7854af53ea24b82c
Sergio Jara Tranamil <jaratran@gmail.com>
2025-11-27 12:21:15 -0300

    Carga en frontend.blade.php de Font Awesome 6 (simple y sin integrity).

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -16,6 +16,9 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 
+		<!-- Usar Font Awesome 6 (simple y sin integrity) -->
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet" referrerpolicy="no-referrer" />
+
 		<!-- 🔹 Aquí cargamos los CSS con estilos de compatibilidad -->
         @vite([
             'resources/css/bootstrap-sim.css',
