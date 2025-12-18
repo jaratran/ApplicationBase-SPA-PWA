6720c8e9faea617b7d44fedd5b35fee883e235ba
Sergio Jara Tranamil <jaratran@gmail.com>
2025-12-18 01:30:57 -0300

    Actualización de versión de SW a v91.

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,7 @@
 // ==================================================================================
 
 // Nombre del caché (cambiará en cada despliegue)
-const CACHE_NAME = "Calidad-v90";
+const CACHE_NAME = "Calidad-v91";
 
 const APP_SHELL = "/build/index.html";
 
